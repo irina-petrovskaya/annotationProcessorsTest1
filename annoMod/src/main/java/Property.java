ca1c658f91345f4437bac0e2363934209f26d5e8
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-04-22 14:50:36 +0300

    classes created

---
+++ annoMod/src/main/java/Property.java
@@ -0,0 +1,9 @@
+package sourceAnnotations;
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 4/22/2015.
+ * Project: annotationProcessorsTest1
+ * *******************************
+ */
+public interface Property {
+}
