496d2d2338b59f469745f71c1cc1a3e10ee4120b
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-04-23 14:59:02 +0300

    add new module

---
+++ clientWithTestsMod/src/main/java/beans/Bean1.java
@@ -0,0 +1,30 @@
+package beans;
+
+import myAnnotations.PrintMe;
+import myAnnotations.Property;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 4/23/2015.
+ * Project: annotationProcessorsTest1
+ * *******************************
+ */
+@PrintMe
+public class Bean1 {
+
+    private String prop1;
+    private String prop2;
+    @Property @PrintMe
+    public String getProp2() {
+        return prop2;
+    }
+    @Property @PrintMe
+    public void writeProp2(String prop2Arg) {
+        prop2 = prop2Arg;
+    }
+    @PrintMe
+    public String readProp1(){
+        return this.prop1;
+    }
+
+}
